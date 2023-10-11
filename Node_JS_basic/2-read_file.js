c2807d3443da9116fd13a39090b1fc9a1024c0bc
Axel Malka <axelmalka@outlook.fr>
2023-10-11 12:41:25 +0000

    synthetise a db

---
+++ Node_JS_basic/2-read_file.js
@@ -0,0 +1,45 @@
+const fs = require('fs');
+
+function countStudents(path) {
+  try {
+    const data = fs.readFileSync(path, 'utf8'); // Read the CSV file synchronously
+
+    // Split the data into lines and filter out empty lines
+    const lines = data.split('\n').filter(line => line.trim() !== '');
+
+    if (lines.length === 0) {
+      console.error('Cannot load the database: The file is empty');
+      return;
+    }
+
+    let students = 0;
+    const fieldCounts = {};
+    const fieldNames = {};
+
+    // Loop through the lines, starting from the second line (index 1)
+    for (let i = 1; i < lines.length; i++) {
+      const line = lines[i];
+      const [firstName, lastName, age, field] = line.split(',');
+
+      if (fieldCounts[field]) {
+        fieldCounts[field]++;
+        fieldNames[field].push(firstName);
+      } else {
+        fieldCounts[field] = 1;
+        fieldNames[field] = [firstName];
+      }
+
+      students++;
+    }
+
+    console.log(`Number of students: ${students}`);
+
+    for (const field in fieldCounts) {
+      console.log(`Number of students in ${field}: ${fieldCounts[field]}. List: ${fieldNames[field].join(', ')}`);
+    }
+  } catch (error) {
+    console.error(`Cannot load the database: ${error.message}`);
+  }
+}
+
+countStudents('database.csv'); // Call the function with the path to your database file
